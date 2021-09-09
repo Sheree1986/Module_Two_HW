13ef31eb510c15c1be188e759a95386b0f642067
Sheree Edmund <87440131+Sheree1986@users.noreply.github.com>
2021-09-09 13:19:35 -0400

    Add files via upload

---
+++ students/src/score.js
@@ -0,0 +1,12 @@
+﻿import React from "react";
+// import App from "./App";
+
+export default function Score(props) {
+  return (
+    <div className="scoreM">
+      Date: {props.score.date}
+      <br />
+      Score: {props.score.score}
+    </div>
+  );
+}
