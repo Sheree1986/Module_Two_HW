22ea0e2bfc28b78be1a8d6052815e30aa3dcc6e5
Sheree Edmund <87440131+Sheree1986@users.noreply.github.com>
2021-09-16 19:02:46 -0400

    Update FloorPlan.js

--- Floor Plan/components/FloorPlan.js
+++ Floor Plan/components/FloorPlan.js
@@ -4,7 +4,7 @@ import LivingRoom from "./LivingRoom";
 import Bedroom from "./Bedroom";
 import Bath from "./Bath";
 
-function FloorPlan(props) {
+function FloorPlan() {
   return (
     
       <>
